c458564fee059e266bf14df080bd4f38f143473a
nico_miarka <nico.miarka@gmail.com>
2022-11-26 20:02:19 +0100

    added hash and object files, simplified state file

--- assets/js/defaults.js
+++ assets/js/defaults.js
@@ -1,12 +1,12 @@
 /* cr.js | MIT License | https://github.com/holgerdell/color-refinement */
 
 /* default URL parameters */
-const state = {
+export const state = {
   n: 50,
   m: 35,
   charge: () => (document.getElementById('main').offsetWidth < 700) ? -100 : -200,
   navbar: true,
-  option: 'rumor',
+  graph: 'rumor',
   seed: () => (Math.seedrandom) ? '' : undefined,
   count: false
 }
