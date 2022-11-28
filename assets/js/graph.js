c816da0e2389aeb3fa4420ce7bea1ee0e8bb64fb
Nicolas Miarka <nico.miarka@gmail.com>
2022-11-28 21:37:07 +0100

    .

--- assets/js/graph.js
+++ assets/js/graph.js
@@ -2,78 +2,77 @@
  * @param {Number} n vertices
  * @return {Graph}
  */
- function create_graph(n) {
-  const graph = { vertices: [], edges: [] }
+function create_graph(n) {
+  const graph = { vertices: [], edges: [] };
   for (let i = 0; i < n; i++) {
-    graph.vertices[i] = { name: i, neighbors: [], level:0}
+    graph.vertices[i] = { name: i, neighbors: [], level: 0 };
   }
-  return graph
+  return graph;
 }
-function randomWalk(n,m,random){
-  const graph = create_graph(n)
+function randomWalk(n, m, random) {
+  const graph = create_graph(n);
   const S = new Set(graph.vertices);
   const visited = new Set();
-  var currentVertex = graph.vertices[Math.floor(random()* graph.vertices.length)]
-  S.delete(currentVertex)
-  visited.add(currentVertex)
+  var currentVertex =
+    graph.vertices[Math.floor(random() * graph.vertices.length)];
+  S.delete(currentVertex);
+  visited.add(currentVertex);
   while (S.size !== 0) {
-    var nextVertex = graph.vertices[Math.floor(random()* graph.vertices.length)]
-    if (!visited.has(nextVertex)){
-      graph.edges.push({ source: currentVertex, target: nextVertex })
-      currentVertex.neighbors.push(nextVertex)
-      nextVertex.neighbors.push(currentVertex)
-      S.delete(nextVertex)
-      visited.add(nextVertex)
+    var nextVertex =
+      graph.vertices[Math.floor(random() * graph.vertices.length)];
+    if (!visited.has(nextVertex)) {
+      graph.edges.push({ source: currentVertex, target: nextVertex });
+      currentVertex.neighbors.push(nextVertex);
+      nextVertex.neighbors.push(currentVertex);
+      S.delete(nextVertex);
+      visited.add(nextVertex);
     }
-    currentVertex = nextVertex
+    currentVertex = nextVertex;
   }
-  return graph
-  
+  return graph;
 }
 /** Sample a random graph G(n,m)
-  * @param {Number} n vertices
-  * @param {Number} m edges
-  * @return {Graph}
-  */
-export function randomGraph (n, m, seed) {
-  
-  const random = (Math.seedrandom) ? (new Math.seedrandom(seed)) : Math.random // eslint-disable-line
-  const graph = randomWalk(n,m,random);
-  /** Generate a list of random integers using sparse Fisher-Yates shuffling */
-
-  return graph
+ * @param {Number} n vertices
+ * @param {Number} m edges
+ * @return {Graph}
+ */
+export function randomGraph(n, m, seed) {
+  const random = Math.seedrandom ? new Math.seedrandom(seed) : Math.random; // eslint-disable-line
+  const graph = randomWalk(n, m, random);
+  return graph;
 }
 
-function crtrees(n,m,random){
-  const maxNumEdges = n * (n - 1) / 2
-  if (n < 0 || m < 0 || m > maxNumEdges) return undefined
-  const graph = create_graph(n)
-  const randomInt = (min, max) => Math.floor(random() * (max - min) + min)
-  const state = {}
+function crtrees(n, m, random) {
+  const maxNumEdges = (n * (n - 1)) / 2;
+  if (n < 0 || m < 0 || m > maxNumEdges) return undefined;
+  const graph = create_graph(n);
+  const randomInt = (min, max) => Math.floor(random() * (max - min) + min);
+  /** Generate a list of random integers using sparse Fisher-Yates shuffling */
+  const state = {};
   for (let i = 0; i < m; i++) {
-    const j = randomInt(i, maxNumEdges)
-    if (!(i in state)) state[i] = i
+    const j = randomInt(i, maxNumEdges);
+    if (!(i in state)) state[i] = i;
     if (!(j in state)) state[j] = j;
-    [state[i], state[j]] = [state[j], state[i]]
+    [state[i], state[j]] = [state[j], state[i]];
   }
 
   /** Cantor's unpairing function
-    * @param {Number} k non-negative integer
-    * @return {NumberPair} returns the k-th non-negative integer pair (x,y)
-    * in the sequence (0,0), (0,1), (1,0), (0,2), (1,1), (2,0), (0,3)...
-    */
-  function unpair (k) {
-    const z = Math.floor((-1 + Math.sqrt(1 + 8 * k)) / 2)
-    return [k - z * (1 + z) / 2, z * (3 + z) / 2 - k]
+   * @param {Number} k non-negative integer
+   * @return {NumberPair} returns the k-th non-negative integer pair (x,y)
+   * in the sequence (0,0), (0,1), (1,0), (0,2), (1,1), (2,0), (0,3)...
+   */
+  function unpair(k) {
+    const z = Math.floor((-1 + Math.sqrt(1 + 8 * k)) / 2);
+    return [k - (z * (1 + z)) / 2, (z * (3 + z)) / 2 - k];
   }
 
   for (let i = 0; i < m; i++) {
-    const [x, y] = unpair(state[i])
-    const u = graph.vertices[x]
-    const v = graph.vertices[n - 1 - y]
-    graph.edges.push({ source: u, target: v })
-    u.neighbors.push(v)
-    v.neighbors.push(u)
+    const [x, y] = unpair(state[i]);
+    const u = graph.vertices[x];
+    const v = graph.vertices[n - 1 - y];
+    graph.edges.push({ source: u, target: v });
+    u.neighbors.push(v);
+    v.neighbors.push(u);
   }
-  return graph
+  return graph;
 }
