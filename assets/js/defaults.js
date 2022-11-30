561b2dde3904637e2c177ab81fdbedd8b23077dd
nico_miarka <nico.miarka@gmail.com>
2022-11-30 19:09:27 +0100

    added addRandomEdges function

--- assets/js/defaults.js
+++ assets/js/defaults.js
@@ -3,7 +3,7 @@
 /* default URL parameters */
 export const state = {
   n: 50,
-  m: 35,
+  m: 40,
   charge: () => (document.getElementById('main').offsetWidth < 700) ? -100 : -200,
   navbar: true,
   graph: 'rumor',
