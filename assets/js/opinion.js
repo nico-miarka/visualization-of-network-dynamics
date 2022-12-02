fce69b857d146a7fb4b6e8d34dce32c1c43eb5c7
nico_miarka <nico.miarka@gmail.com>
2022-12-02 20:39:17 +0100

    added TODOs changed changeVertex

--- assets/js/opinion.js
+++ assets/js/opinion.js
@@ -1,23 +1,36 @@
-/** Generalize for h-neighbors (h-Majority) */
+import {counter} from './lib/counter.js'
+
+/**TODO Generalize for h-neighbors (h-Majority) */
+import {highlightVertex, highlightVertices, grayOutGraph,drawVertexColor, resetHighlightGraph} from './visuals.js'
 function pickVertex(graph,random){
     return graph.vertices[Math.floor(random() * graph.vertices.length)]
 }
 
-function pickNeighbor(vertex,random){
-    return vertex.neighbors[Math.floor(random() * vertex.neighbors.length)]
+function pickNeighbor(vertex,random,majority){
+    const shuffled = vertex.neighbors.sort(() => 0.5 - random());
+    return shuffled.slice(0,majority)
 }
 
-function changeOpinion (vertex, neighbor){
-    vertex.level = neighbor.level
+function changeOpinion (vertex, neighbors){
+    /** TODO add select max opinion from neighbors (what if multiply opinions with same amount? random?) */
+    
 }
 
-export function changeVertex (graph,seed){
+export async function changeVertex (graph,seed,h){
     const random = Math.seedrandom ? new Math.seedrandom(seed) : Math.random; // eslint-disable-line
     const vertex = pickVertex(graph, random)
-    console.log(vertex)
-    const neighbor = pickNeighbor(vertex, random)
-    changeOpinion(vertex,neighbor)
-    console.log(neighbor)
-    console.log(vertex)
+    await sleep(500)
+    grayOutGraph()
+    highlightVertex(vertex)
+    const neighbors = pickNeighbor(vertex, random,1)
+    await sleep(500)
+    highlightVertices(neighbors)
+    await sleep(500)
+    changeOpinion(vertex,neighbors)
+    drawVertexColor(vertex)
+    await sleep(3000)
+    resetHighlightGraph()
 }
-
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
